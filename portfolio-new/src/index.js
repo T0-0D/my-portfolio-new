ddbbc2fc41e7d368e2148afe0e32e266f4520fb4
T0-0D <tacwingman015@gmail.com>
2022-02-19 19:24:37 +0900

    add new directory

--- portfolio-new/src/index.js
+++ portfolio-new/src/index.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-
 import './index.css';
-import Header from './components/Header'
-import Footer from  './components/Footer'
-import Home from './Home'
-import Activities from './Activities'
-import Music from './Music'
-
-
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
-    <Router>
-        <Header />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/Activities" component={Activities} />
-        <Route exact path="/Music" component={Music} />
-      </Router>
-      <Footer />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
